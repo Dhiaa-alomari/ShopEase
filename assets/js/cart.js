734e53b662d02efae1146a29425d1057bfa5b585
dhiaa-alomari <dhiaaalomari81@gmail.com>
2025-11-28 14:03:39 +0000

    add function for showing all products that in cart

---
+++ assets/js/cart.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+  let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+renderCart();
+
+// ======== Render Cart ===========
+  function renderCart() {
+    $("#cartItems").empty();
+    let total = 0;
+
+    // Disable checkout button if cart is empty
+    $("#checkoutBtn").prop("disabled", true);
+    // Enable checkout button if cart has items
+    if(cart.length !== 0) {
+      $("#checkoutBtn").prop("disabled", false);
+    }
+
+    cart.forEach((item, i) => {
+      let subtotal = item.price * item.qty;
+      total += subtotal;
+      $("#cartItems").append(`  
+        <tr>
+          <td>${item.name}</td>
+          <td>$&nbsp;${item.price}</td>
+          <td class="d-flex align-items-center no-wrap">
+            <button class="btn btn-sm btn-outline-secondary decrement-qty" data-index="${i}">-</button>
+            <span class="mx-2">${item.qty}</span>
+            <button class="btn btn-sm btn-outline-secondary increment-qty" data-index="${i}">+</button>
+          </td>
+          <td>${subtotal}$</td>
+          <td><button class="btn btn-danger btn-sm remove-item" data-index="${i}">Delete</button></td>
+        </tr>
+      `);
+    });
+
+    // Calculate total for all items
+    total = cart.reduce((sum, item) => sum + item.price * item.qty, 0);
+    $("#totalPrice").text(total);
+  }
+  
+})
